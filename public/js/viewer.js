dc08b0751092b8eebbd4b1077c595b6bedf816be
Ben Zhang <bzhang.2003@gmail.com>
2016-04-05 16:30:37 -0400

    basic result display

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,7 +1,35 @@
+var hbsrc = document.getElementById("result-template").innerHTML;
+var hbtemplate = Handlebars.compile(hbsrc);
 
+function displayResults( entries ){
+	if(entries.ok && entries.courses.length > 0){
+		showResult( hbtemplate(entries) );
+	}
+	else{
+		showResult("<span>[No results found.]</span>");
+	}
+}
+
+function searchKeyPress(e){
+    e = e || window.event;
+    if (e.keyCode == 13){
+        document.getElementById('search-button').click();
+        return false;
+    }
+    return true;
+}
+
+function showResult( str ){
+	var result_box = document.getElementById("results");
+	result_box.innerHTML = str;
+
+}
 
 function runSearch( str ){
 	console.log("searching for", str);
+
+	showResult("[Searching...]");
+
 	var xhr = new XMLHttpRequest();
 	xhr.onload = function(){
 		if(this.status != 200){
@@ -11,7 +39,7 @@ function runSearch( str ){
 		if(res.ok){
 			console.log("Search completed.");
 			console.log(res);
-			
+			displayResults(res);
 		} else {
 			console.log("Search failed: " + res.reason, 4000);
 		}
@@ -22,5 +50,6 @@ function runSearch( str ){
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-	document.getElementById("search_button").onclick = function(){ runSearch( document.getElementById("search").value ) };
+	document.getElementById("search-button").onclick = function(){ runSearch( document.getElementById("search").value ) };
+	document.getElementById("search").onkeypress = searchKeyPress;
 });
